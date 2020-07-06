29cfbeefade29298aba6434e8a4e37292a1d7533
virmon <virmontumulak@gmail.com>
2020-07-06 17:33:22 -0500

    add menu

--- src/edu/miu/cs/cs525/reversi/controller/BoardUIController.java
+++ src/edu/miu/cs/cs525/reversi/controller/BoardUIController.java
@@ -1,6 +1,11 @@
 package edu.miu.cs.cs525.reversi.controller;
 
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuItem;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -11,4 +16,27 @@ public class BoardUIController implements Initializable {
     public void initialize(URL url, ResourceBundle resourceBundle) {
 
     }
+
+    @FXML
+    public void handleNewGame() {
+        System.out.println("Start New Game...");
+    }
+
+    @FXML
+    public void handleSaveState() {
+        System.out.println("Save game state...");
+    }
+
+    @FXML
+    public void handleLoadState() {
+        System.out.println("Load previous game state...");
+    }
+
+    @FXML
+    public void handleExitGame() {
+        System.out.println("Exiting game...");
+        Platform.exit();
+        System.exit(0);
+    }
+
 }
