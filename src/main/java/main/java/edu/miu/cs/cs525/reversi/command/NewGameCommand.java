c899642950ccabfa5ac67a860e76b1e8c9370fc7
virmon <virmontumulak@gmail.com>
2020-07-13 16:35:55 -0500

    add new game command

--- src/main/java/main/java/edu/miu/cs/cs525/reversi/command/NewGameCommand.java
+++ src/main/java/main/java/edu/miu/cs/cs525/reversi/command/NewGameCommand.java
@@ -1,17 +1,18 @@
 package main.java.edu.miu.cs.cs525.reversi.command;
 
+import main.java.edu.miu.cs.cs525.reversi.action_adapters.BoardViewTemplate;
 import main.java.edu.miu.cs.cs525.reversi.monitor.MainForm;
 
 public class NewGameCommand implements Command {
-	private MainForm service;
+	private BoardViewTemplate b;
 	
-	public NewGameCommand(MainForm service) {
-		this.service = service;
+	public NewGameCommand(BoardViewTemplate b) {
+		this.b = b;
 	}
 
 	@Override
 	public void execute() {
-//		service
+		b.newGame();
 	}
 
 }
